c91e29ed03845eef984bab8a13136a216b7b0b2b
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-10-10 23:13:36 +0200

    Created SearchBar component
    It still needs to implement the Yelp interface, but it has been set up to use the variable names in Yelp API documentation

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,33 @@
 import React from "react";
+
+const sortingOptions = {
+    "Best Match" : "best_match",
+    "Highest Ratings": "rating",
+    "Most Reviews": "review_count"
+};
+
+const SearchBar = () => {
+    const displaySortingOptions = () => {
+        return Object.keys(sortingOptions).map((sortingOption) => {
+            let sortingOptionKey = sortingOptions[sortingOption];
+            return <li key={sortingOptionKey}>{sortingOption}</li>
+        });
+    };
+
+    return (
+        <div>
+            <div>
+                <ul>{displaySortingOptions()}</ul>
+            </div>
+            <div>
+                <input placeholder="Search restaurants"/>
+                <input placeholder="Where?"/>
+            </div>
+            <div>
+                <a>Search</a>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBar;
